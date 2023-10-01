82a60a69296c3f3d14e900f4a865579048755057
Shlok <shlokstha99@gmail.com>
2023-10-01 22:27:49 +0545

    learn about RestAPI CRUD

--- index.js
+++ index.js
@@ -13,40 +13,61 @@ server.use(express.json());
 // server.use(express.urlencoded());
 server.use(express.static("public"));
 
-server.use(morgan("default"));
-// server.use((req, res, next) => {
-//   console.log(req.method, req.ip, req.hostname);
-//   next();
-// });
-
-const auth = (req, res, next) => {
-  // console.log(req.query);
-  if (req.body.password == "123") {
-    next();
-  } else {
-    res.sendStatus(401);
-  }
-};
-
-//REq.params
+//Req.params
 server.get("/product/:id", (req, res) => {
   console.log(req.params);
   res.json({ type: "GET" });
 });
 
 //API - Endpoint - Route
-server.get("/", (req, res) => {
-  res.json({ type: "GET" });
+
+//Products
+//API ROOT , BASEURL http://localhost:8000 different baseurl eg google.com/api/v2
+//Read API GET /products
+server.get("/products", (req, res) => {
+  res.json(products);
 });
-server.post("/", auth, (req, res) => {
-  res.json({ type: "POST" });
+//Read API for single product GET /products/:id
+server.get("/products/:id", (req, res) => {
+  const id = +req.params.id;
+  const singleProduct = products.find((p) => p.id === id);
+  res.json(singleProduct);
 });
-server.put("/", (req, res) => {
-  res.json({ type: "PUT" });
+
+//Create API Post /products
+server.post("/products", (req, res) => {
+  console.log(req.body);
+  products.push(req.body);
+  res.json(req.body);
 });
-server.delete("/", (req, res) => {
-  res.json({ type: "DELETE" });
+
+//Update API for single product PUT /products/:id
+server.put("/products/:id", (req, res) => {
+  const id = +req.params.id;
+  const productIndex = products.findIndex((p) => p.id === id);
+  products.splice(productIndex,1,{...req.body, id:id})
+  res.status(201).json();
 });
+//Update API for single product PATCH /products/:id
+server.patch("/products/:id", (req, res) => {
+  const id = +req.params.id;
+  const productIndex = products.findIndex((p) => p.id === id);
+  const oldproduct = products[productIndex];
+  products.splice(productIndex,1,{...oldproduct, ...req.body})
+  res.status(201).json();
+});
+
+
+
+//Delete API for single product Delete /products/:id
+server.delete("/products/:id", (req, res) => {
+  const id = +req.params.id;
+  const productIndex = products.findIndex((p) => p.id === id);
+  const oldproduct = products[productIndex];
+  products.splice(productIndex,1)
+  res.status(201).json(oldproduct);
+});
+
 
 server.get("/demo", (req, res) => {
   // res.sendStatus(202).send(error);
