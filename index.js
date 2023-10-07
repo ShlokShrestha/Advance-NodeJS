a3e9e2091c40fa4969247db552ac5eb3eff73cc1
Shlok <shlokstha99@gmail.com>
2023-10-07 13:50:55 +0545

    learn about auth with jwt

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
-const express = require("express");
+require('dotenv').config()
+const express = require('express');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
 const server = express();
-const productRouter = require("./routes/product");
-// const morgan = require("morgan");
-const mongoose = require("mongoose");
+const path = require('path');
+const productRouter = require('./routes/product')
+const userRouter = require('./routes/user')
+const jwt = require('jsonwebtoken');
+const authRouter = require("./routes/auth");
+const fs = require('fs');
+const publicKey = fs.readFileSync(path.resolve(__dirname,'./public.key'),'utf-8')
 
 main().catch((err) => console.log(err));
 
@@ -10,15 +17,36 @@ async function main() {
   await mongoose.connect("mongodb://127.0.0.1:27017/ecommerce");
   console.log("DataBase Connected");
 }
+const auth = (req,res,next)=>{
+  
+  try{
+    const token = req.get('Authorization').split('Bearer ')[1];
+    console.log(token);
+    var decoded = jwt.verify(token,publicKey );
+    if(decoded.email){
+      next()
+    }else{
+      res.sendStatus(401)
+    }
+  }catch(err){
+    res.sendStatus(401)
+  }
+  console.log(decoded)
+  
 
+};
 
 //environement-variable
-require("dotenv").config();
-//Build-in middleware (body parser JSON) for when body request is undefined
 server.use(express.json());
-// server.use(express.urlencoded());
-server.use(express.static(process.env.PUBLIC_DIR));
-server.use("/api", productRouter);
+server.use(express.urlencoded({extendend:true}));
+// server.use(morgan('default'));
+// server.use(express.static(path.resolve(__dirname,process.env.PUBLIC_DIR)));
+server.use('/auth',authRouter.router)
+server.use('/products',auth,productRouter.router);
+server.use('/users',auth,userRouter.router);
+server.use('*',(req,res)=>{
+    res.sendFile(path.resolve(__dirname,'build','index.html'))
+})
 
 server.listen(process.env.PORT, () => {
   console.log("server started at 8000");
